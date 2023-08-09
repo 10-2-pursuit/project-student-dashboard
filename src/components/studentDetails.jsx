f0778967118d49571b8489e53655d64272f50f7f
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-09 17:42:01 -0400

    toggle button and viewmore function is working

--- src/components/studentDetails.jsx
+++ src/components/studentDetails.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import React from 'react';
+
 
 const StudentDetails = ({student}) => {
 const [viewMore, setViewMore]= useState(false);
@@ -11,7 +13,7 @@ const isOnTrack = student.certifications.resume && student.certifications.linked
 
 const assignmentsPercentage = student.cohort.scores.assignments * 100;
 const projectsPercentage = student.cohort.scores.projects * 100;
-const assessmentsPercentage = student.cohort.scores.assessment * 100; 
+const assessmentsPercentage = student.cohort.scores.assessments * 100; 
 
 
     return (
@@ -22,15 +24,15 @@ const assessmentsPercentage = student.cohort.scores.assessment * 100;
          <p>UserName: {student.username}</p>
          <p>Birthday: {student.dob}</p> 
          <p>Status: {isOnTrack ? "On Track" : "Failing Foward"}</p>
-         <button onclick={toggleView}> 
+         <button className="toggle-button" onClick={toggleView}> 
          {viewMore ? "Hide Details" : "Show Details"}
          </button>
 
         {viewMore && (
             <div className="additional-info">
-                <p>Assigments Score: {assignmentsPercentage}%</p>
-                <p>Project Score: {projectsPercentage}%</p>
-                <p>Assesments Score: {assessmentsPercentage}%</p>
+                <p>Assigments Score: {assignmentsPercentage.toFixed(2)}%</p>
+                <p>Project Score: {projectsPercentage.toFixed(2)}%</p>
+                <p>Assesments Score: {assessmentsPercentage.toFixed(2)}%</p>
                 <p>Resume Certification: {student.certifications.resume ? "✅ " : "❌"}</p>
                 <p>Resume Certification: {student.certifications.resume ? "✅ " : "❌"}</p>
                 <p>LinkedIn Certification:{student.certifications.linkedin ? "✅" : "❌"}</p>
