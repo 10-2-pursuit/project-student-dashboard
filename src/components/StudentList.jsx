9caf54bf656ba98a866dc4446027ddf7fe209f58
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-09 16:15:59 -0400

    added code to displlay whether student is on track or off track

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -7,11 +7,15 @@ const StudentList =  ({students, onSelectStudent}) => {
         {students.map(student => (
           <div key={student.id} className='student'>
             <img src= {student.profilePhoto} alt={`${student.names.preferredName}'s profile`} />
+            <br/> 
             <p>Name: {student.names.preferredName}</p>
+            <br/>
+            <p>UserName: {student.username}</p>
+            <br/>
             <p>Birthday: {student.dob}</p>
             <button onClick={() => onSelectStudent(student)}>Show Details</button>
       </div>
-      ))}ea
+      ))}
       </div>
     );
         };
--- src/components/studentDetails.jsx
+++ src/components/studentDetails.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const StudentDetails = ({student}) => {
+const isOnTrack = student.certifications.resume && student.certifications.linkedin && student.certifications.github && student.certifications.mockInterview && student.codewars.current.total > 600; 
+
     return (
         <div className="student-details">
          <h2>Student Details</h2>
          <img src={student.profilePhoto} alt={`${student.names.preferredName}'s profile`}/>
          <p>Name: {student.names.preferredName}</p>
          <p>UserName: {student.username}</p>
-         <p>Birthday: {student.dob}</p>
+         <p>Birthday: {student.dob}</p> 
+         <p>Status: {isOnTrack ? "On Track" : "Failing Foward"}</p>
         </div>
 
     );
-}
+};
 
 export default StudentDetails;
