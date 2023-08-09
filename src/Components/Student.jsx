e45a27582148a779e667ff5232d33485b358105a
Nugget-Meister <tremainecamacho@gmail.com>
2023-08-08 20:11:49 -0400

    working state for show/hide details

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Student = ({student}) => {
+    const name = student.names
+
+    const [ShowDetails, setShowDetails] = useState(false)
+
+    const handleShowDetails = () => {
+        setShowDetails(!ShowDetails)
+    }
+
     return (
-        <div className='Student'>
+        <div className='Student' id={student.id}>
             <img src={student.profilePhoto} alt="" />
-            <span>spooky</span>
+            <div className='StudentRight'>
+                <h3>{`${name.preferredName} ${name.middleName} ${name.surname}`}</h3>
+                <span>{`${student.username}`}</span><br />
+                <span><span className='Glow'>Birthday: </span>{`${student.dob}`}</span><br />
+                <span 
+                    className='Glow ShowMore'
+                    onClick={handleShowDetails}
+                    >{ShowDetails ? "Hide Details..." : "Show Details..."}</span><br />
+                {ShowDetails ? (
+                <>
+                supae
+                </>
+                ) : null}
+            </div>
         </div>
     );
 }
