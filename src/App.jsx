18cee0397fd18bdd6f4e60fc425b6543ba7fa0b8
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-11 10:19:02 -0400

    created the function that will map through the array of students cohorts using .map

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,19 @@ import data from "./data/data.json";
 
 const App = () => {
   const [selectedStudent, setSelectedStudent] = useState(null);
-  const [notes, setNotes] = useState({});
-
+ 
   const handleSelectedStudent = (student) => {
     setSelectedStudent(student);
   };
 
-  const addNote = (studentId, newNote) => {
-    setNotes((previousNotes) => ({
-      ...previousNotes,
-      [studentId]: [...(previousNotes[studentId] || []), newNote],
-    }));
-  };
-
   return (
     <div className="dashboard">
-      <h1>All Students</h1>
-      <p>Total students: {data.length}</p>{/*Display Total Count of Students */}
+      <h1>DashBoard</h1>
+      <p className="total-count">Total students: {data.length}</p>{/*Display Total Count of Students */}
       <StudentList students={data} onSelectStudent={handleSelectedStudent} />
       {selectedStudent && (
         <div className="student-details">
-          {<StudentDetails student={selectedStudent} />}
+          <StudentDetails student={selectedStudent}/>
         </div>
       )}
     </div>
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import StudentDetails from './studentDetails';
+import React from "react";
+import { useState } from "react";
 
-const StudentList =  ({students, onSelectStudent}) => {
-    return (
-      <div className="student-list">
-        {students.map(student => (
-          <div key={student.id} className='student'>
-            <img src= {student.profilePhoto} alt={`${student.names.preferredName}'s profile`} />
-            <br/> 
+const StudentList = ({ students, onSelectStudent }) => {
+  const cohortNamesSet = [
+    ...new Set(students.map((student) => student.cohort.cohortCode)),
+  ];
+  const readableCohortName = Array.from(cohortNamesSet).map((cohortCode) => {
+    const parts = cohortCode.match(/([A-Za-z]+)(\d+)/);
+    return `${parts[1]} ${parts[2]}`;
+  });
+  return (
+    <div className="student-list">
+      <h2>All Students</h2>
+      <ul>
+        {students.map((student) => (
+          <div key={student.id} className="student">
+            <img
+              src={student.profilePhoto}
+              alt={`${student.names.preferredName}'s profile`}
+            />
+            <br />
             <p>Name: {student.names.preferredName}</p>
-            <br/>
+            <br />
             <p>UserName: {student.username}</p>
-            <br/>
+            <br />
             <p>Birthday: {student.dob}</p>
-            <button onClick={() => onSelectStudent(student)}>Show Student</button>
-      </div>
-      ))}
-      </div>
-    );
-        };
+            <button onClick={() => onSelectStudent(student)}>
+              Show Student
+            </button>
+          </div>
+        ))}
+      </ul>
+      <div className="cohort-list">
+  <h2>Available Cohorts</h2>
+  <ul>
+    {readableCohortName.map((cohortNames, index) => (
+      <li key={index}>{cohortNames}</li>
+    ))}
+  </ul>
+</div>
+    </div>
+  );
+};
 
 export default StudentList;
