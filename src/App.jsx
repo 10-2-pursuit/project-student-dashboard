873d042ac10fb525f944b0029cf9d6f36d204cfb
DaviiJones <davonbridgett@pursuit.org>
2023-09-25 07:51:31 -0400

    attempt to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import cohortListData from '/Users/davonbridgett/Documents/project-student-dashboard/src/data/data.json';
 import React from 'react';
+
+import Dashboard from './components/dashboard';
 import Header from './components/header';
 import Sidebar from './components/sidebar';
-import Dashboard from './components/Dashboard';
-import cohortListData from '/Users/davonbridgett/Documents/project-student-dashboard/src/data/data.json';
+
 import './index.css';
+
 function App() {
   return (
     <div>
